235252b5c840eb19ff2a56c24eba67f5adb148a4
Ketan <ketantoo@gmail.com>
2020-07-11 20:55:07 +0530

    sign in page appbar

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -6,6 +6,16 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Time Tracker"),
+        elevation: 2.0,
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[Text("Sign In")],
+        ),
       ),
     );
   }
