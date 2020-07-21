f30f860b978c48d16bda8f61e3964077e9c4d71a
Ketan <ketantoo@gmail.com>
2020-07-21 11:45:47 +0530

    sign out implemented

---
+++ lib/app/homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatelessWidget {
+  final VoidCallback onSignOut;
+
+  const HomePage({Key key, this.onSignOut}) : super(key: key);
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Homepage"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: _signOut,
+            child: Text(
+              "Log Out",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
