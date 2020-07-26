93087277292cf99d69ec1a4135a958937c2011c7
Ketan <ketantoo@gmail.com>
2020-07-27 00:10:26 +0530

    refractored sign in flow

--- lib/app/sign_in/landing_page.dart
+++ lib/app/sign_in/landing_page.dart
@@ -11,29 +11,6 @@ class LandingPage extends StatelessWidget {
     this.auth,
   }) : super(key: key);
 
-  // @override
-  // void initState() {
-  //   // Todo: this method must be synchronous
-  //   _checkCurrentUser();
-  //   super.initState();
-  //   widget.auth.onAuthStateChanged.listen((user) {
-  //     print("user: ${user?.uid}");
-  //   });
-  // }
-
-  // Future<void> _checkCurrentUser() async {
-  //   User user = await widget.auth.currentUser();
-  //   print(user);
-  //   _updateUser(user);
-  // }
-
-  // void _updateUser(User user) {
-  //   print(user);
-  //   setState(() {
-  //     _user = user;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
