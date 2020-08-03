357f229e2d33f73c3d040c1625928b5631658a7f
Ketan <ketantoo@gmail.com>
2020-08-03 23:31:56 +0530

    added email and password fiels

--- lib/app/sign_in/email_signin_form.dart
+++ lib/app/sign_in/email_signin_form.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        decoration: InputDecoration(
+          labelText: "Email",
+          hintText: "username@email.com",
+        ),
+      ),
+      TextField(
+        decoration: InputDecoration(
+          labelText: "Password",
+          // hintText: "Password",
+        ),
+        obscureText: true,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("akak"),
+    return Padding(
+      padding: EdgeInsets.all(
+        16.0,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
     );
   }
 }
