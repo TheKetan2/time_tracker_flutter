809db2557160c1c09c4894f57a7fc0ef72856c98
Ketan <ketantoo@gmail.com>
2020-08-04 15:13:17 +0530

    added textcontrolle

--- lib/app/sign_in/email_signin_form.dart
+++ lib/app/sign_in/email_signin_form.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter/app/custom_raised_buttons/form_custom_raised_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
+
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit() {
+    print(
+        "email: ${_emailController.text},\nPassword: ${_passwordController.text}");
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.register
+          ? EmailSignInFormType.signIn
+          : EmailSignInFormType.register;
+    });
+
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? "Sign in"
+        : "Create an account";
+
+    final secondayText = _formType == EmailSignInFormType.signIn
+        ? "Need an account? Register"
+        : "Have and account? Sign in";
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: "Email",
           hintText: "username@email.com",
@@ -14,6 +51,7 @@ class EmailSignInForm extends StatelessWidget {
         height: 8,
       ),
       TextField(
+        controller: _passwordController,
         decoration: InputDecoration(
           labelText: "Password",
           // hintText: "Password",
@@ -24,10 +62,8 @@ class EmailSignInForm extends StatelessWidget {
         height: 8,
       ),
       FormSubmitButton(
-        text: "Enter",
-        onPressed: () {
-          print("daba");
-        },
+        text: primaryText,
+        onPressed: _submit,
       ),
       // RaisedButton(
       //   onPressed: () {},
@@ -37,8 +73,8 @@ class EmailSignInForm extends StatelessWidget {
         height: 8,
       ),
       FlatButton(
-        onPressed: () {},
-        child: Text("Create an account"),
+        onPressed: _toggleFormType,
+        child: Text(secondayText),
       )
     ];
   }
