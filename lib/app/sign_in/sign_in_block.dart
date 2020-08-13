f6c32ac0344f018024e57986044fe48732236e49
Ketan <ketantoo@gmail.com>
2020-08-13 12:05:04 +0530

    adding sing in methods in signin Bloc itself

--- lib/app/sign_in/sign_in_block.dart
+++ lib/app/sign_in/sign_in_block.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
 
+import 'package:time_tracker_flutter/services/auth.dart';
+import 'package:flutter/foundation.dart';
+
 class SignInBlock {
+  final AuthBase auth;
   final StreamController<bool> _isLoadingController = StreamController<bool>();
+
+  SignInBlock(this.auth);
 
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
@@ -9,5 +15,30 @@ class SignInBlock {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  // abstract class AuthBase {
+  // Stream<User> get onAuthStateChanged;
+  // Future<User> currentUser();
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+
+  Future<User> signInAnonymously() async =>
+      await _signIn(auth.signInAnonymously);
+
+  // Future<User> signInWithEmailAndPassword(String email, String password);
+  // Future<User> createUserWithEmailAndPassword(String email, String password);
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+  // Future<User> signInWithFacebook();
+//   Future<void> signOut();
+// }
 }
