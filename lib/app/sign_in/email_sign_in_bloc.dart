7f392bc9e5b3e4265b9eb271f3a913e37c8bdc4e
Ketan <ketantoo@gmail.com>
2020-08-14 15:31:14 +0530

    updating model

--- lib/app/sign_in/email_sign_in_bloc.dart
+++ lib/app/sign_in/email_sign_in_bloc.dart
@@ -6,9 +6,29 @@ class EmailSignInBloc {
   final StreamController<EmailSignInModel> _modelController =
       StreamController<EmailSignInModel>();
 
+  EmailSignInModel _model = EmailSignInModel();
+
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
 
   void dispose() {
     _modelController.close();
+  }
+
+  void updateWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+
+    _modelController.add(_model);
   }
 }
