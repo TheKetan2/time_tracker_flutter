0cb3d27afe2b1bd4cdc8009eca20523602652ef3
Ketan <ketantoo@gmail.com>
2020-08-25 03:18:00 +0530

    setting up database provider

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter/app/home/jobs_page.dart';
 import 'package:time_tracker_flutter/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter/services/auth.dart';
+import 'package:time_tracker_flutter/services/database.dart';
 
 class LandingPage extends StatelessWidget {
   @override
@@ -16,7 +17,11 @@ class LandingPage extends StatelessWidget {
             if (user == null) {
               return SignInPage.create(context);
             }
-            return JobsPage();
+            return Provider<Database>(
+                builder: (_) => FirestoreDatabase(
+                      uid: user.uid,
+                    ),
+                child: JobsPage());
           } else {
             return Scaffold(
               body: Center(
